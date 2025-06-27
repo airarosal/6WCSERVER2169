1dcca55a533cd65b6f6b6e0ab1d53e681b4b3c06
airarosal <airamaetrosal14@gmail.com>
2025-06-27 13:01:43 +0800

    Add files via upload

---
+++ PRELIMS/app.js
@@ -0,0 +1,14 @@
+var myLogModule = require('./UTILITY/Log.js');
+
+// myLogModule.warning('Warning node not configured..');
+// myLogModule.error('Error: Node encountered an error..');
+// myLogModule.info('Node running...');
+
+// var msg = require('./utility/Message.js');
+// console.log(msg);
+
+// var person = require('./UTILITY/data.js');
+// console.log(person.firstName + ' ' + person.lastName);
+
+var msg = require('./UTILITY/Log.js');
+msg('Hello World');
