cffb49e24a5c6cccf6c37efbffc6f69c75ea85fc
airarosal <airamaetrosal14@gmail.com>
2025-07-18 12:34:59 +0800

    Add files via upload

---
+++ PRELIMS/WEEK5/modules/gallery_module.js
@@ -0,0 +1,5 @@
+function gallery() {
+    return `<h1>This is the Gallery Page</h1>`;
+}
+
+module.exports = gallery;
---
+++ PRELIMS/WEEK5/modules/not_found_module.js
@@ -0,0 +1,5 @@
+function not_found(name) {
+    return `<p>Invalid Request</p>`;
+}
+
+module.exports = not_found;
